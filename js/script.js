0238600309e73fbc2cf54648b8645764c5c83d50
Sayed Mohamed <113357344+sayedmohamed7@users.noreply.github.com>
2024-09-20 09:48:41 +0300

    Fetch data from TMDB API

--- js/script.js
+++ js/script.js
@@ -2,12 +2,27 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+
+// Fetch data from TMDB API
+async function fetchAPIData(endpoint) {
+  const API_KEY = '2635839629d3c675dd8efe3cc11cb4f8';
+  const API_URL = 'https://api.themoviedb.org/3/';
+
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+
+  const data = await response.json();
+  return data;
+}
+
 // Highlight active link
 const highlightActiveLink = () => {
   const links = document.querySelectorAll('.nav-link');
   links.forEach((link) => {
     link.getAttribute('href') === global.currentPage &&
       link.classList.add('active');
+      global.currentPage === '/index.html'&& links[0].classList.add('active');
   });
 };
 
@@ -16,7 +31,6 @@ const init = () => {
   switch (global.currentPage) {
     case '/':
     case '/index.html':
-      console.log('Home');
       break;
     case '/shows.html':
       console.log('Shows');
