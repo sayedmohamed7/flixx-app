3749cb0c005ac21c6cead75ebc6512fea29c6543
Sayed Mohamed <113357344+sayedmohamed7@users.noreply.github.com>
2024-09-28 15:01:25 +0300

    Display backdrop image to details page

--- js/script.js
+++ js/script.js
@@ -73,6 +73,10 @@ async function displayPopularShows() {
 async function displayMovieDetails() {
   const movieId = window.location.search.split('=')[1];
   const movie = await fetchAPIData(`movie/${movieId}`);
+
+  // Overlay for background image
+  displayBackgroundImage('movie', movie.backdrop_path);
+
   const div = document.createElement('div');
 
   div.innerHTML = `<div class="details-top">
@@ -124,9 +128,26 @@ async function displayMovieDetails() {
             .join(', ')}.</div>
         </div>`;
   document.getElementById('movie-details').appendChild(div);
-
-  console.log(movie);
 }
+
+// Display backdorp on details page
+const displayBackgroundImage = (type, backgroundPath) => {
+  const overlayDiv = document.createElement('div');
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+  overlayDiv.style.backgroundSize = 'cover';
+  overlayDiv.style.backgroundPosition = 'center';
+  overlayDiv.style.backgroundRepeat = 'no-repeat';
+  overlayDiv.style.height = '100vh';
+  overlayDiv.style.width = '100vw';
+  overlayDiv.style.position = 'absolute';
+  overlayDiv.style.top = '0';
+  overlayDiv.style.left = '0';
+  overlayDiv.style.zIndex = '-1';
+  overlayDiv.style.opacity = '0.1';
+  type === 'movie'
+    ? document.querySelector('#movie-details').appendChild(overlayDiv)
+    : document.querySelector('#show-details').appendChild(overlayDiv);
+};
 
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
