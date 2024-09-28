431f51c97e40e234a12cc2d6393423b5df50dc0b
Sayed Mohamed <113357344+sayedmohamed7@users.noreply.github.com>
2024-09-28 17:44:05 +0300

    Display tv-shows details

--- js/script.js
+++ js/script.js
@@ -130,6 +130,70 @@ async function displayMovieDetails() {
   document.getElementById('movie-details').appendChild(div);
 }
 
+// Display show details
+async function displayShowDetails() {
+  const showId = window.location.search.split('=')[1];
+  const show = await fetchAPIData(`tv/${showId}`);
+
+  // Overlay for background image
+  displayBackgroundImage('show', show.backdrop_path);
+
+  const div = document.createElement('div');
+
+  div.innerHTML = `<div class="details-top">
+          <div>
+            ${
+              show.poster_path
+                ? `<img
+              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+              class="card-img-top"
+              alt="${show.original_name}"
+            />`
+                : `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${show.original_name}"
+            />`
+            }
+          </div>
+          <div>
+            <h2>${show.original_name}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${show.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
+            <p>${show.overview}</p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+            ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${
+              show.homepage
+            }" target="_blank" class="btn">Visit Show Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Show Info</h2>
+          <ul>
+            <li><span class="text-secondary">Number Of Episodes: </span> ${
+              show.number_of_episodes
+            }</li>
+            <li>
+              <span class="text-secondary">Last Episode To Air:</span> ${
+                show.last_episode_to_air.name
+              }
+            </li>
+            <li><span class="text-secondary">Status:</span> ${show.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">${show.production_companies
+            .map((comp) => `<span>${comp.name}</span>`)
+            .join(', ')}.</div>
+        </div>`;
+  document.getElementById('show-details').appendChild(div);
+}
+
 // Display backdorp on details page
 const displayBackgroundImage = (type, backgroundPath) => {
   const overlayDiv = document.createElement('div');
@@ -197,7 +261,7 @@ const init = () => {
       displayMovieDetails();
       break;
     case '/tv-details.html':
-      console.log('TV Details');
+      displayShowDetails();
       break;
     case '/search.html':
       console.log('Search');
