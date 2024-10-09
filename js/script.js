b83ad36aa31f5e5227bf2e44ac1516dd97d876e3
Sayed Mohamed <113357344+sayedmohamed7@users.noreply.github.com>
2024-10-09 23:57:19 +0300

    Display slider movies

--- js/script.js
+++ js/script.js
@@ -213,6 +213,53 @@ const displayBackgroundImage = (type, backgroundPath) => {
     : document.querySelector('#show-details').appendChild(overlayDiv);
 };
 
+// Display slider movies
+async function displaySlider() {
+  const { results } = await fetchAPIData('movie/now_playing');
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('swiper-slide');
+    div.innerHTML = `
+            <a href="movie-details.html?id=${movie.id}">
+              <img src="https://image.tmdb.org/t/p/w500${
+                movie.poster_path
+              }" alt="${movie.title}" />
+            </a>
+            <h4 class="swiper-rating">
+              <i class="fas fa-star text-secondary"></i> ${movie.vote_average.toFixed(
+                1
+              )} / 10
+            </h4>`;
+    document.querySelector('.swiper-wrapper').appendChild(div);
+
+    initSwiper();
+  });
+}
+
+const initSwiper = () => {
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInterAction: false,
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+      },
+      700: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    },
+  });
+};
+
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
   const API_KEY = '2635839629d3c675dd8efe3cc11cb4f8';
@@ -253,6 +300,7 @@ const init = () => {
     case '/':
     case '/index.html':
       displayPopularMovies();
+      displaySlider();
       break;
     case '/shows.html':
       displayPopularShows();
